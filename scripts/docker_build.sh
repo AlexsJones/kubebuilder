b41b371b34f12ab704c739ce46e0559be1b8cd09
alexsjones <tibbar@freedommail.ch>
2017-10-15 18:24:48 +0100

    updated

--- scripts/docker_build.sh
+++ scripts/docker_build.sh
@@ -1,6 +1,6 @@
 version="v1"
 pushd files
-docker build -t kubebuilder:$version .
+docker build --no-cache=true -t kubebuilder:$version .
 popd
 pushd k8s
 sed -ie "s/kubebuilder:v1/kuberbuilder:$version/g" ./environments/build.yaml
